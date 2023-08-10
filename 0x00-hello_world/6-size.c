eced6736a07452f3adb6c21084d639918bb0ca79
Mpendulo-M <mpendulomasango58@gmail.com>
2023-08-10 03:16:26 -0700

    update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
-
-
 /**
- * main - Entry Point
- * 
+ * main - Entry point
+ *
  * Return: Always 0 (Success)
  */
 
-int main(void){
-
-
-printf("Size of a char: 1 byte(s)\nSize of an int: 4 byte(s)\nSize of a long int: 4 byte(s)\nSize of a long long int: 8 byte(s)\nSize of a float: 4 byte(s)");
-
-
+int main(void)
+{
+printf("Size of a char: 1 byte(s)\nSize of an int: 4 byte(s)\n");
+printf("Size of a long int: 4 byte(s)\nSize of a long long int: 8 byte(s)\n");
+printf("Size of a float: 4 byte(s)\n");
 
 return 0;
 }
