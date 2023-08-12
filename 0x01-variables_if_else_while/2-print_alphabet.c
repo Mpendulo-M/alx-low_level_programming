2c9acfa664059dacc26d5b1693c0735ccb6e4e67
Mpendulo-M <mpendulomasango58@gmail.com>
2023-08-11 18:22:03 -0700

    update

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+    char cc = 'a';
+
+    while (cc <= 'z')
+    {
+        putchar(cc);
+        cc++;
+    }
+
+    putchar('\n');
+
+    return (0);
+}
+
