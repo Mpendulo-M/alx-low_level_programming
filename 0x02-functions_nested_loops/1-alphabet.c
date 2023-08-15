a3f1c24fb79e86972433f3183f537abd0eed9c4b
Mpendulo-M <mpendulomasango58@gmail.com>
2023-08-15 05:42:52 -0700

    update

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * main - Entry point
  *
@@ -20,12 +21,5 @@ void print_alphabet(void)
 
 }
 
-int main(void)
-{
 
-	print_alphabet();
-
-
-	return (0);
-}
 
