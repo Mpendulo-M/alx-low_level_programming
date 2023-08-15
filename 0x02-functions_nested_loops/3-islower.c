a89c2de97c4019bf86fefecb25ffc15f14f0db66
Mpendulo-M <mpendulomasango58@gmail.com>
2023-08-15 07:02:43 -0700

    update

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,15 +3,15 @@
 /**
  *_islower - Entry point
  *
- * Return: Always 1 (Success) 
- *
- * _islower(int c) - determines if c is lowercase or not
+ *Return: Always 1 (Success 
+ *c parameter is an integer representing ascii code
+ *_islower(int c) - determines if c is lowercase or not
  */
 int _islower(int c)
 {
 
 
-	if (c>= 97 && c<= 122)
+	if (c >= 97 && c <= 122)
 		return (1);
 	else
 		return (0);
