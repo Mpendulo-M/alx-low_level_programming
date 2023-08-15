55733fc21619b76d32ecc4be2ce0177089bc0423
Mpendulo-M <mpendulomasango58@gmail.com>
2023-08-15 08:31:43 -0700

    update

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * @x: integer to be tested 
+ * Return postive x is x is negtive or positive or return 0
+ *
+ */
+
+int _abs(int x);
+{
+	if (x < 0)
+		return (-x);
+	else if (x == 0)
+		return (0);
+	else
+		return (x);
+
+}
