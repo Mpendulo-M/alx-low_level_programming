c7cfe847b5b8697236db15c8ffc4ec061677079b
Mpendulo-M <mpendulomasango58@gmail.com>
2023-08-15 07:47:13 -0700

    update

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,16 +2,25 @@
 /**
  * print_sign - prints the sign of a number
  *@n: the number to be checked
- *Return: 1 if number is postive, return 0 if number is zero, return -1 if number is negative
+ *Return: 1 if number is postive, 0 if number is zero or -1 if negative
  */
 
 int print_sign(int n)
 {
 	if (n > 0)
+	{
+		_putchar('+');
 		return (1);
+	}
 	else if (n == 0)
+	{
+		_putchar('0');
 		return (0);
+	}
 	else
+	{
+		_putchar('-');
 		return (-1);
+	}
 
 }
