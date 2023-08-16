7af869ad647cbf9bb1d0338505ee2d1e4da37624
Mpendulo-M <mpendulomasango58@gmail.com>
2023-08-16 04:27:45 -0700

    update

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	int i = 0;
+
+	postive_or_negative(i);
+
+	return (0);
+}
