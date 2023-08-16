cb03713bafa328122c4d4fb5fe0feb146cb29ab2
Mpendulo-M <mpendulomasango58@gmail.com>
2023-08-15 18:49:41 -0700

    update

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - adds two numbers together
+ *@a: first integer to be added
+ *@b: second integer to be added
+ * Return: answer (successful)
+ */
+
+int add(int a, int b)
+{
+	answer = a + b;
+
+	return (answer);
+}
