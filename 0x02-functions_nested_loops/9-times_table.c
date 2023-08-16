02dbc30e21f8bf493155631b21937daf65359296
Mpendulo-M <mpendulomasango58@gmail.com>
2023-08-15 18:43:34 -0700

    update

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -21,7 +21,7 @@ void times_table(void)
 				_putchar(d + '0');
 				_putchar(n + '0');
 			}
-			else 
+			else
 			{
 				if (y != 0)
 			{
@@ -29,7 +29,7 @@ void times_table(void)
 				_putchar(32);
 				_putchar(32);
 			}
-			
+
 			_putchar(z + '0');
 			}
 	}
