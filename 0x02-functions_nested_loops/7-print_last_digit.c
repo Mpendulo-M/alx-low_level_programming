a6228106675be0176797eb7a96b17328fe75d380
Mpendulo-M <mpendulomasango58@gmail.com>
2023-08-15 19:55:34 -0700

    update

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,6 +1,5 @@
 #include "main.h"
 /**
- *
  * print_last_digit - prints last digit of an integer
  * @x: integer being tested
  * Return: Result (Successful)
@@ -9,7 +8,7 @@
 int print_last_digit(int x)
 {
 	int result;
-       
+
 	result = x % 10;
 
 	if (result < 0)
