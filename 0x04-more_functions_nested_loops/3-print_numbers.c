8a85da860b3b6d5de487f8c65e0f3314f62679ef
Mpendulo-M <mpendulomasango58@gmail.com>
2023-08-17 06:54:12 -0700

    update

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers from zero to nine
+ */
+void print_numbers(void)
+{
+	int c = '0';
+
+	while (c <= '9')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+
+
+}
