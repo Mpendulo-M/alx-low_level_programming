54dda486b365c874e71eff577b7dbacf8e413441
Mpendulo-M <mpendulomasango58@gmail.com>
2023-08-17 07:07:32 -0700

    update

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints numbers between 0-9 without 2and4
+ */
+void print_most_numbers(void)
+{
+	int c = '0';
+
+	while (c <= '9')
+	{
+		if (c != '2' || c != '4')
+			_putchar(c);
+		c++;
+	}
+
+	_putchar('\n');
