5c98d5d0c69c17293c1429c88a419fb72d6708ba
Mpendulo-M <mpendulomasango58@gmail.com>
2023-08-17 03:23:59 -0700

    update

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,24 +1,14 @@
 #include "main"
 /**
- *
- *
- *
+ * _isupper - checks if letter is upper case
+ *@c: letter to be checked 
+ * Return: 0 if uppercase else return 1
  */
 int _isupper(int c)
 {
 	if ((c >= 65) && (c <= 90))
-	{
-		_putchar(c);
-		_putchar(': ');
-		_putchar('0');
 		return (0);
-	}
-	else if ((c >= 97) && (c <= 122))
-	{
-		_putchar(c);
-		_putchar(': ');
-		_putchar('1');
-		return (1);
-	}
 
+	else if ((c >= 97) && (c <= 122))
+		return (1);
 }
