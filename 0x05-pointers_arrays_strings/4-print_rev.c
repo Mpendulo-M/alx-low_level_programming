c6ce26d41e24a78dc122dd94391cbcaf2c58b5d9
Mpendulo-M <mpendulomasango58@gmail.com>
2023-08-21 06:18:03 -0700

    update

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *print_rev - print a string in reverse
+ *@s: the string to be reversed
+ */
+
+
+void print_rev(char *s)
+{
+	int i;
+	int count = 0;
+
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+	}
+	s--;
+
+	for (i = count; i >= 0; i--)
+	{
+		_putchar(*s);
+		s--;
+	}
+
+	_putchar('\n');
+}
