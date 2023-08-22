c0c32cb0d48dae4575edf20c6da4c4e25772be99
Mpendulo-M <mpendulomasango58@gmail.com>
2023-08-21 20:26:48 -0700

    update

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *puts_halft - prints half the string
+ *@str: string to be halved
+ */
+void puts_half(char *str)
+{
+	int i, x;
+	int count = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+		count++;
+
+
+	if ((count%2) == 1)
+		i = ((count-1)/2);
+	else
+		i = count/2;
+
+	for (i = n; str[a] != '\0'; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+
+
+
+}
