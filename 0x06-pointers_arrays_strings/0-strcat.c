0da1b2bdba7a420c34bc430301c4018e7388ab06
Mpendulo-M <mpendulomasango58@gmail.com>
2023-08-23 04:34:30 -0700

    update

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,9 +3,24 @@
  *_strcat - adds strings together
  *@dest: string one
  *@src: string two
+ *Return: new string
  */
 char *_strcat(char *dest, char *src)
 {
-	*dest = *dest + *src;
+	char *holder = dest;
 
+	while (*holder != '\0')
+	{
+		holder++;
+	}
+
+	while (*src != '\0')
+	{
+		*holder = *src;
+		holder++;
+		src++;
+	}
+	*holder = '\0';
+	dest = holder;
+	return (dest);
 }
