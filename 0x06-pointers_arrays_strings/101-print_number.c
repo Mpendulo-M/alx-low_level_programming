c9f436809043e5667870ae710d867acab6538485
Mpendulo-M <mpendulomasango58@gmail.com>
2023-08-24 09:27:50 -0700

    update

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_number - prints an integer using putchar
+ *@n: digit to be printed
+ *
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n * -1;
+	}
+	_putchar((n/10) + '0');
+	_putchar((n%10) + '0');
+
+
+
+
+
+
+
+
+}
