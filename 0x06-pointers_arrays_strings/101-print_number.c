d2fc6c2e0dafd9ecb5b139ec52b406e976810b13
Mpendulo-M <mpendulomasango58@gmail.com>
2023-08-24 10:17:23 -0700

    update

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,19 +7,55 @@
 
 void print_number(int n)
 {
+
+	int T = 0;
+	int h = 0;
+	int t = 0;
+
 	if (n < 0)
 	{
+		n = -n;
 		_putchar('-');
-		n = n * -1;
 	}
-	_putchar((n / 10) + '0');
-	_putchar((n % 10) + '0');
 
+	while (n >= 1000)
+	{
+		n = n - 1000;
+		T++;
+	}
+	while (n >= 100)
+	{
+		n = n - 100;
+		h++;
+	}
+	while (n >= 10)
+	{
+		n = n - 10;
+		t++;
+	}
 
-
-
-
-
+	if (n == 0)
+		_putchar(48);
+	else if (T != 0)
+	{
+		_putchar(T + '0');
+		_putchar(h + '0');
+		_putchar(t + '0');
+		_putchar(n + '0');
+	}
+	else if (h != 0)
+	{
+		_putchar(h + '0');
+		_putchar(t + '0');
+		_putchar(n + '0');
+	}
+	else if (t != 0)
+	{
+		_putchar(t + '0');
+		_putchar(n + '0');
+	}
+	else
+		_putchar(n + '0');
 
 
 }
