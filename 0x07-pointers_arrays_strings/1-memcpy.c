80073f3e65944ab879d802574dbf47116a3c02f2
Mpendulo-M <mpendulomasango58@gmail.com>
2023-08-29 14:07:36 -0700

    update

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *_memcpy - copies memory area
+ *@dest: memory where data is stored
+ *@src: memory to copy to
+ *@n: number of bytes
+ *Return: new copied memory
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int count, holder;
+
+	count = 0;
+	holder = n;
+
+	while (count < holder)
+	{
+		dest[count] = src[count];
+		count++;
+	}
+	
+	return (dest);
+
+}
+ 
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -1,1 +1,3 @@
 0. memset - Write a function that fills memory with constant byte.
+
+1. memcpy - A function that copies memory area
