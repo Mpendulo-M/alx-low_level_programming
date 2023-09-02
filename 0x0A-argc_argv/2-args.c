11f3139fafc184fd91c59cfc82dbf0fccc461d7b
Mpendulo-M <mpendulomasango58@gmail.com>
2023-09-02 13:30:23 -0700

    update

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - print all arguments
+ *@argc: argument counter
+ *@argv: argument vector
+ *Return: 0 (successful)
+ */
+
+int main(int argc, char **argv)
+{
+	while (argc--)
+		printf("%s\n", *argv++);
+
+
+	return (0);
+}
