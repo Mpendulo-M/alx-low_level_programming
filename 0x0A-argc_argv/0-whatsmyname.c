5dcdfa7c5a0e8240fc3260054893d1ca58b9a61e
Mpendulo-M <mpendulomasango58@gmail.com>
2023-09-01 20:01:27 -0700

    update

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints file name
+ *@argc: number
+ *@argv: array
+ *Rreturn: 0 successful
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+
+	return (0);
+}
+
