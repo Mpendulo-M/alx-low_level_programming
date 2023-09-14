b0699fccfdcdaa1aca69b6c200423d63b225dd28
Mpendulo-M <mpendulomasango58@gmail.com>
2023-09-14 09:33:45 -0700

    update

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -16,7 +16,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		x = va_arg(x, char *);
+		x = va_arg(args, char *);
 
 		if (x == NULL)
 			printf("(nil)");
