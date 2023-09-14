6f96e4317c7a11c31a1ea4a3a9abc5dba77d8dda
Mpendulo-M <mpendulomasango58@gmail.com>
2023-09-14 06:59:12 -0700

    update

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ *print_numbers - prints numbers passed as arguments
+ *@separator: the sepatator
+ *@n: number of arguments
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int x;
+	unsigned int i;
+
+	va_list args;
+
+	va_start(args, n);
+
+	for (i = 1; i < n; i++)
+	{
+		x = va_arg(args, int);
+		printf("%d%c ", x, *separator);
+	}
+	x = va_arg(args, int);
+	printf("%d\n", x);
+
+
+}
