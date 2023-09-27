8c5a13f91456f9bfb6d5c068fc341edc4a10241a
Mpendulo-M <mpendulomasango58@gmail.com>
2023-09-27 05:51:29 -0700

    final commit on puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string
+ * @s: String to be printed
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+
+	_putchar(s[0]);
+	s++;
+	_puts_recursion(s);
+
+	if (*s == '\0')
+		_putchar('\n');
+
+}
