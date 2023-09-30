86fb013fdbeae580c910293a86660346cad49b83
Mpendulo-M <mpendulomasango58@gmail.com>
2023-09-29 18:11:02 -0700

    initial commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *main - prints number of arguments passed
+ *@argc: argument counter
+ *@argv: argument vector
+ *Return: 0 (successful)
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	argc--;
+	printf("%d\n", argc);
+
+	return (0);
+}
