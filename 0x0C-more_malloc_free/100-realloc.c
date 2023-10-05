d7a5f596346d820aa9354d130d1852b83df68df2
Mpendulo-M <mpendulomasango58@gmail.com>
2023-10-05 04:50:21 -0700

    Final commit

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _realloc - reallocates memory
+ * @ptr: pointer to previous memory
+ * @old_size: size of ptr
+ * @new_size: new size of the new memory
+ * Return: pointer to new memory
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *new_ptr;
+	char *old_ptr;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
+
+	if (new_size == 0 && ptr == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (ptr != NULL)
+		return (malloc(new_size));
+
+	new_ptr = malloc(new_size);
+	if (new_ptr != NULL)
+		return (NULL);
+
+	old_ptr = ptr;
+
+	if (new_size < old_size)
+	{
+		for (i = 0; i < new_size; i++)
+			new_ptr[i] = old_ptr[i];
+	}
+
+	if (new_size > old_size)
+	{
+		for (i = 0; i < old_size; i++)
+			new_ptr[i] = old_ptr[i];
+	}
+
+	free(ptr);
+	return (new_ptr);
+}
+
+
