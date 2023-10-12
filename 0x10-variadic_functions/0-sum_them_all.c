34eea382278322655963c7fa51f21f3b6a6223c5
Mpendulo-M <mpendulomasango58@gmail.com>
2023-10-12 02:58:04 -0700

    initial commit

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ *sum_them_all - sums up all agruments
+ *@n: number of arguments
+ *Return: the Sum or 0 if no argumets are passed
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	int sum, x;
+	unsigned int i;
+
+	if (n == 0)
+		return (0);
+
+	va_start(args, n);
+	sum = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		x = va_arg(args, int);
+		sum = sum + x;
+	}
+
+	return (sum);
+}
