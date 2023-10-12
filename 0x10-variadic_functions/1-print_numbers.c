766fabf86704b1b7d1e5ecb70838b528cdcae1b1
Mpendulo-M <mpendulomasango58@gmail.com>
2023-10-12 08:08:51 -0700

    update

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -8,12 +8,13 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	unsigned int x;
+
+	unsigned int i, x;
 	va_list args;
 
 	va_start(args, n);
 
-	for (unsigned int i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		x = va_arg(args, unsigned int);
 		if (i != 3 && separator != NULL)
