481ccc6d7222ac2cf0221ba8077e26ad6f1c14ef
Mpendulo-M <mpendulomasango58@gmail.com>
2023-10-19 07:59:44 -0700

    initial commit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,19 +11,16 @@ size_t print_list(const list_t *h)
 {
 	size_t nodes = 0;
 
-	while (h != NULL)
+	while (h)
 	{
-	if (h->str != NULL)
-		printf("[%u} %s\n", h->len, h->str);
-	else
-		printf("[0] (nil)\n");
-
-	h = h->next;
-	nodes++;
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		s++;
 	}
 
 	return (nodes);
-
-
-
 }
+
