62a39ae02d020c2780331558dc210657769d10c8
Mpendulo-M <mpendulomasango58@gmail.com>
2023-10-24 12:36:02 -0700

    update

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -13,7 +13,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *newHead = NULL;
 	listint_t *temp = *head;
 
-	new = malloc(sizeof(listint_t));
+	newHead = malloc(sizeof(listint_t));
 	
 	if (!new)
 		return (NULL);
