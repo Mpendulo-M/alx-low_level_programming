4ec16508565e159187dced333d40f20aa0e84a6c
Mpendulo-M <mpendulomasango58@gmail.com>
2023-10-24 14:18:28 -0700

    update

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * pop_listint - deletes the head
+ * @head: pointer to the first element
+ *
+ * Return: the data inside the element
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int i;
+
+	if (!head || !*head)
+		return (0);
+
+	
+	i = (*head)->n;
+	temp = (*head)->next;
+	free(*head);
+	*head = temp;
+
+	return (i);
+}
---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * get_nodeint_at_index - returns the node at specific place
+ * @head: first node
+ * @index: index of the node to return
+ *
+ * Return: pointer to the specific node
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+	listint_t *temp = NULL;
+	temp = head;
+
+	while (temp && i < index)
+	{
+		temp = temp->next;
+		i = i + 1;
+	}
+	if (temp)
+		return (temp);
+
+	return (NULL);
+}
