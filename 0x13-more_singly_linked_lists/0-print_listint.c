8eadccc2a144c68bd0800ffced3afa8237d63605
Mpendulo-M <mpendulomasango58@gmail.com>
2023-10-24 12:01:24 -0700

    initial commit

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * print_listint - prints elements of a linked list
+ * @h: linked list
+ *
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+ size_t nodes = 0;
+
+ while (h != NULL)
+ {
+ printf("%d\n", h->data);
+ nodes++;
+ h = h->link;
+ }
+
+ return (nodes);
+}
+
