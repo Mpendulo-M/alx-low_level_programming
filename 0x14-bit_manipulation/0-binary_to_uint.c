32f4ef7aafe084d67eaaf5b1fca2050de856fbf3
Mpendulo-M <mpendulomasango58@gmail.com>
2023-10-26 07:00:47 -0700

    initial commit

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * binary_to_uint - convert binary to unsigned int
+ * @b: Binary
+ * Return: unsigned int or 0 if NULL
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i = 0;
+	unsigned int answer = 0;
+
+	if (b == NULL)
+		return (0);
+  
+	while (b[i] != '\0')
+	{
+		if (b[i] == '0' || b[i] == '1')
+			answer = (answer << 1) | (b[i] - '0');
+		else
+			return (0);
+		i++;
+	}
+
+	return (answer);
+}
