be177ca5d9045295e125e89276fc029a74ee215b
Mpendulo-M <mpendulomasango58@gmail.com>
2023-10-30 09:42:04 -0700

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,10 @@
 #include "main.h"
+/**
+ * create_file - creates a file
+ * @filename: name of file
+ * @text_content: what to wrie into file
+ * Return: 1 successful
+ */
 
 int create_file(const char *filename, char *text_content)
 {
